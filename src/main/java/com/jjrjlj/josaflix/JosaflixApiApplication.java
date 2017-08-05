b689f1ec07f8d5c689405746df940e1402d2176c
luiz jacó <luizjacomn@gmail.com>
2017-08-05 13:21:58 -0300

    Primeiro commit

---
+++ src/main/java/com/jjrjlj/josaflix/JosaflixApiApplication.java
@@ -0,0 +1,12 @@
+package com.jjrjlj.josaflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JosaflixApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JosaflixApiApplication.class, args);
+	}
+}
